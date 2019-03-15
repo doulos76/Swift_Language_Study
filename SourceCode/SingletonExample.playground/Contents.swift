6a1daba28f89ea07c9e392752ed8be008b63ac7c
doulos76 <inojyes@gmail.com>
2019-03-15 12:28:51 +0900

    add singleton example

---
+++ SourceCode/SingletonExample.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+let userDefaults = UserDefaults.standard
+
+class Car {
+  var color = "Red"
+  
+  static let singletonCar = Car()
+}
+
+let myCar = Car.singletonCar
+myCar.color = "Blue"
+
+let yourCar = Car.singletonCar
+print(yourCar.color)
+
+class A {
+  init() {
+    Car.singletonCar.color = "Brown"
+  }
+}
+
+class B {
+  init() {
+    print(Car.singletonCar.color)
+  }
+}
+
+let a = A()
+let b = B()
