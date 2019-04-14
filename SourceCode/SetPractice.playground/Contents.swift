a9ecd5271d1eb0fae65a348bba5e900a851f02e7
doulos76 <inojyes@gmail.com>
2019-04-14 15:33:34 +0900

    add set practice

---
+++ SourceCode/SetPractice.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//var someArray: Array<Int> = [1, 2, 3, 1]
+var someSet: Set<Int> = [1, 2, 3, 1]
+
+someSet.contains(1)
+someSet.contains(11)
+
+someSet.insert(5)
+someSet
+
+someSet.remove(1)
+someSet
+
