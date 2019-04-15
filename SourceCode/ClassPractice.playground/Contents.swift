82754237252113218f1e8237dbb8565894e95c2a
doulos76 <inojyes@gmail.com>
2019-04-15 10:39:03 +0900

    study class vs struct

--- SourceCode/ClassPractice.playground/Contents.swift
+++ SourceCode/ClassPractice.playground/Contents.swift
@@ -6,6 +6,11 @@ struct PersonStruct {
   var fullName: String {
     return "\(firstName) \(lastName)"
   }
+  
+  mutating func uppercaseName() {
+    firstName = firstName.uppercased()
+    lastName = lastName.uppercased()
+  }
 }
 
 class PersonClass {
@@ -20,4 +25,32 @@ class PersonClass {
   var fullName: String {
     return "\(firstName) \(lastName)"
   }
+  
+  func uppercaseName() {
+    firstName = firstName.uppercased()
+    lastName = lastName.uppercased()
+  }
 }
+
+var person1 = PersonStruct(firstName: "Jason", lastName: "Lee")
+let person2 = person1
+
+person1.firstName = "Jay"
+person1.firstName
+person2.firstName
+
+var person3 = PersonClass(firstName: "Jason", lastName: "Lee")
+var person4 = person3
+
+person3.firstName = "Jay"
+person3.firstName
+person4.firstName
+
+person4 = PersonClass(firstName: "Bob", lastName: "Lee")
+person4.firstName
+person3.firstName
+
+person4 = person3
+person4.firstName
+person3.firstName
+
